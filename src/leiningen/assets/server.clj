bc4cc6b059995923f3eb70faa8b9895b7a785aab
Eldar Gabdullin <eldargab@gmail.com>
2014-08-24 22:10:28 +0400

    support fresh builds

--- src/leiningen/assets/server.clj
+++ src/leiningen/assets/server.clj
@@ -8,11 +8,6 @@
             [hiccup.page :refer [html5]]
             [leiningen.core.eval :refer [eval-in-project]]))
 
-(defn trim-slashes [s]
-  (-> s
-      (string/replace #"^/" "")
-      (string/replace #"/$" "")))
-
 (defmacro call [f & args]
   `((find-var '~f) ~@args))
 
@@ -22,26 +17,42 @@
 
 (defn render-package-page [{pkg :main :as build} optimize?]
   (hiccup/html
-   (html5
-    [:html
-     [:head
-      [:title (or (:title pkg) (:name pkg))]
-      [:style (:css-out build)]
-      (if-not optimize?
-        [:script {:src "/goog/base.js"}])
-      [:script (:js-out build)]
-      (if-not optimize?
-        [:script (:js-require-call build)])]
-     [:body (list
-              (render-main-html pkg)
-              [:script (:js-main-call build)])]])))
+    (html5
+      [:html
+       [:head
+        [:title (or (:title pkg) (:name pkg))]
+        [:style (-> build :build :css :css)]
+        (if-not optimize?
+          [:script {:src "/goog/base.js"}])
+        [:script (-> build :build :cljs :js)]
+        (if-not optimize?
+          [:script (-> build :build :cljs :require-call)])]
+       [:body (list
+                (render-main-html pkg)
+                [:script (-> build :build :cljs :main-call)])]])))
 
-(defn send-package [name opts optimize?]
-  (let [build (call dar.assets.builders/development name
-                (if optimize?
-                  (call dar.assets.builders.cljs/production opts)
-                  (call dar.assets.builders.cljs/development opts)))]
-    {:body (render-package-page build optimize?)}))
+(defn option [name q]
+  (.contains q name))
+
+(def last-cljs-options (atom nil))
+
+(defn send-package [name opts query]
+  (let [optimize? (option "optimize" query)
+        fresh? (or
+                 (option "fresh" query)
+                 (not= @last-cljs-options (:cljs opts)))]
+    (reset! last-cljs-options (:cljs opts))
+    (when fresh?
+      (call dar.assets/delete-build-dir opts)
+      (call dar.assets.builders.cljs/clear-env))
+    {:body (render-package-page
+             (call dar.assets/build
+               name
+               (find-var 'dar.assets.builders.development/build)
+               (if optimize?
+                 (call dar.assets.builders.cljs/set-production-options opts)
+                 (call dar.assets.builders.cljs/set-development-options opts)))
+             optimize?)}))
 
 (defn text [status body]
   {:status status
@@ -51,28 +62,30 @@
 (defn send-exeption [ex]
   (text 500 (str "500 Internal Server Error\n\n" (pst-str ex))))
 
+(defn trim-slashes [s]
+  (-> s
+    (string/replace #"^/" "")
+    (string/replace #"/$" "")))
+
 (defn base-handler [project opts]
   (fn [req]
-    (let [path (-> req :uri (java.net.URI.) (.getPath) (trim-slashes))
-          optimize? (.contains
-                      (or (:query-string req) "")
-                      "optimize")]
+    (let [path (-> req :uri (java.net.URI.) (.getPath) (trim-slashes))]
       (if (= "" path)
         (text 200 "Welcome to the assets dev server!")
         (let [f (io/file (:build-dir opts) path)]
           (if (and (.exists f) (not (.isDirectory f)))
             {:body f}
             (eval-in-project (assoc project :eval-in :classloader)
-                             `(try
-                                (require
-                                  '[dar.assets]
-                                  '[dar.assets.builders]
-                                  '[dar.assets.builders.cljs])
-                                (if (call dar.assets/assets-edn-url ~path)
-                                  (send-package ~path ~opts ~optimize?)
-                                  (text 404 "404 Not Found"))
-                                (catch java.lang.Throwable ex#
-                                  (send-exeption ex#))))))))))
+              `(try
+                 (require
+                   '[dar.assets]
+                   '[dar.assets.builders.development]
+                   '[dar.assets.builders.cljs])
+                 (if (call dar.assets/assets-edn-url ~path)
+                   (send-package ~path ~opts ~(or (:query-string req) ""))
+                   (text 404 "404 Not Found"))
+                 (catch java.lang.Throwable ex#
+                   (send-exeption ex#))))))))))
 
 (defn wrap-stacktrace [handler]
   (fn [req]
@@ -83,6 +96,6 @@
 
 (defn run [project opts]
   (let [handler (-> (base-handler project opts)
-                    (wrap-file-info)
-                    (wrap-stacktrace))]
+                  (wrap-file-info)
+                  (wrap-stacktrace))]
     (run-jetty handler {:port (:server-port opts)})))
