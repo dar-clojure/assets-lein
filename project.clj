ebbd29e59117c614be654f9e969eb3965b7abde4
Eldar Gabdullin <eldargab@gmail.com>
2014-10-12 19:47:49 +0400

    Release 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dar/assets-lein "0.0.2"
+(defproject dar/assets-lein "0.0.3"
   :description "Leiningen plugin for building assets"
   :url "https://github.com/dar-clojure/assets-lein"
   :license {:name "Eclipse Public License"
@@ -7,5 +7,5 @@
   :dependencies [[clj-stacktrace "0.2.7"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [ring/ring-core "1.2.2"]
-                 [dar/assets "0.0.2"]
+                 [dar/assets "0.0.3"]
                  [dar/container "0.2.0"]])
